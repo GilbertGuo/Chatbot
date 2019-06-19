5f1e73dee9543661924a63899a2dce057a8eba8a
Yiwei Guo <yiwei.guo@mail.utoronto.ca>
2019-06-19 17:07:09 -0400

    modified the whole user interface and delete the icons

--- UI/ui/src/components/Chatbot.js
+++ UI/ui/src/components/Chatbot.js
@@ -1,33 +1,87 @@
 import React from 'react';
 import './Chatbot.css';
-import green from './green_icon.png';
-import yellow from './yellow_icon.png';
-import btn from './send-button.png';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: '50px',
+        padding: theme.spacing(3, 2),
+    },
+
+    flex:{
+        display: 'flex',
+        height: '300px',
+        alignItems: 'center'
+    },
+
+    chatWindow: {
+        width: '100%',
+        padding: '10px'
+    },
+
+    chatBox: {
+        width: '85%'
+    },
+
+    button: {
+        width: '15%'
+    }
+}));
 
 function Chatbot() {
+
+    const classes = useStyles();
+    const [textValue, changeTextValue] = React.useState();
+
     return (
         <div className="chat_bot">
-            <div>
+            <Paper className={classes.root}>
+                <Typography variant="h4" component="h4">
+                    Chatbot
+                </Typography>
+                <Typography variant="h5" component="h5">
+                    Topic Placeholder
+                </Typography>
+                <div className={classes.flex}>
 
-            </div>
-            <div className="message_container">
-                <div className="container">
-                    <img src={green}/>
-                    <p>Hello. How are you today?</p>
-                    <span className="time-right">11:00</span>
+                    <div className={classes.chatWindow}>
+                        {
+                            [{from:'chatbot', msg:'hi'}].map((chat, i) =>(
+                                <div className={classes.flex} key={i}>
+                                    <Chip label={chat.from} className={classes.chip} variant="outlined" />
+                                    <Typography variant='p'>{chat.msg}</Typography>
+                                </div>
+
+                            ))
+                        }
+
+                    </div>
                 </div>
-                <div className="container darker">
-                    <img src={yellow} className="right"/>
-                    <p className="right">Hey! I'm fine. Thanks for asking!</p>
-                    <span className="time-left">11:01</span>
+                <div className={classes.flex}>
+                    <TextField
+                        label="Type message..."
+                        className={classes.chatBox}
+                        value={textValue}
+                        onChange={e => changeTextValue(e.target.value)}
+                    />
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Send
+                    </Button>
+
                 </div>
-            </div>
-            <div className="input_container">
-                <input type="text" className="input_area" placeholder="Type Message..."/>
-                <button className="btn"><img src={btn} /></button>
-            </div>
+            </Paper>
         </div>
     );
 }
+
+// function btnWasClicked(e){
+//     e.preventDefault();
+//     alert('clicked');
+// }
 
 export default Chatbot;
