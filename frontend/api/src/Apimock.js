3f84995bfacdd03dad0634f5d454cb283127c456
liangyo6 <yongli.liang@mail.utoronto.ca>
2019-06-20 16:24:16 -0400

    1. Modified Backend to allow CORS 2. Added frontend API to do basic GET request and is able to fetch the content and display back to the browser. 3. Created mock data and unit test for axios requests

---
+++ frontend/api/src/Apimock.js
@@ -0,0 +1,30 @@
+import React,{useState,useEffect,Component} from "react";
+import axios from "axios";
+
+export default function Apimock(props){
+    const [data, setData]=useState(null);
+
+    useEffect(
+        ()=>{
+            let mounted=true;
+
+            const loadData=async () => {
+                const response =await axios.get(props.url);
+                if(mounted){
+                    setData(response.data);
+                }
+            };
+            loadData();
+
+            return () =>{
+                mounted=false;
+            };
+        },
+        [props.url]
+    );
+    if(!data){
+        return <div data-testid="waiting">Please wait a sec!</div>;
+    }
+
+    return <div data-testid="transmiting">{data.response}</div>;
+}
---
+++ frontend/api/src/components/Api.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+
+class Api extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            result:''
+
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:9000/helloworld')
+            .then(response=>{
+                this.setState({result:response.data})
+            })
+            .catch(error=>{
+                this.setState({errorMsg:'Error retrieving data'})
+            })
+    }
+
+
+    render() {
+        const {result}=this.state;
+        return (
+            <div>
+                Result of GET:
+                {
+                    result ?
+                        <div>{result.response}</div>:null
+                }
+            </div>
+        )
+    }
+
+
+}
+export default Api;
